40f294b841a84109e18bd8d887025d95a3320299
dgtball <acmaxov@gmail.com>
2026-06-19 16:27:53 +0400

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from tabulate import tabulate
 
 # ---------- КОНФИГУРАЦИЯ ----------
 API_TOKEN = os.getenv("6231234093:AAFqG9gpCNrsr2RLoi2lNrMtvSg1pnEIt3Y")  # Токен берём из переменных окружения
-BASE_URL = os.getenv("BASE_URL")    # Например, https://your-service.onrender.com
+BASE_URL = os.getenv("https://mtb-0rk3.onrender.com")    # Например, https://your-service.onrender.com
 TOP_N = 10
 
 if not API_TOKEN or not BASE_URL:
