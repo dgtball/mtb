74a7547b7889c9f7aec64d8ef32f5573f266c9ea
dgtball <acmaxov@gmail.com>
2026-07-02 18:26:55 +0400

    3.8.18

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 # ---------- ВЕРСИЯ ----------
-VERSION = "3.8.17"
+VERSION = "3.8.18"
 # ---------- ТОКЕНЫ ----------
 API_TOKEN = os.getenv("BOT_TOKEN")
 TINKOFF_TOKEN = os.getenv("TITN")
